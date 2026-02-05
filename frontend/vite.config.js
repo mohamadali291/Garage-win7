669d7dae8ad87a95e17e9cc1f70ae83998b88ddc
mohamad_ghoush <mohamadghoush545@gmail.com>
2026-02-05 14:35:36 +0200

    2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ const CSP_META =
   '<meta http-equiv="Content-Security-Policy" content="default-src \'self\'; script-src \'self\'; style-src \'self\' \'unsafe-inline\'; img-src \'self\' data:; connect-src \'self\' http://localhost:* http://127.0.0.1:*; font-src \'self\'; base-uri \'self\'">';
 
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
     {
